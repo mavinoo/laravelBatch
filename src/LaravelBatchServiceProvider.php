dc870ac66295da6ef4f45805060bde99a245bec5
Ibrahim Sakr <ebrahimes@gmail.com>
2019-02-23 14:12:30 +0200

    add gitignore

--- src/LaravelBatchServiceProvider.php
+++ src/LaravelBatchServiceProvider.php
@@ -7,6 +7,9 @@ use Illuminate\Database\DatabaseManager;
 
 class LaravelBatchServiceProvider extends ServiceProvider
 {
+    /**
+     * @updateedBy Ibrahim Sakr <ebrahimes@gmail.com>
+     */
     public function register()
     {
         $this->app->bind(Batch::class, function ($app){
