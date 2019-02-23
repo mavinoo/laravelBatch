3fb1e0cc6b1c25ff52d73bb13b93dd04602c01d2
Ibrahim Sakr <ebrahimes@gmail.com>
2019-02-23 14:08:03 +0200

    update service providers

--- src/LaravelBatchServiceProvider.php
+++ src/LaravelBatchServiceProvider.php
@@ -3,14 +3,14 @@
 namespace Mavinoo\LaravelBatch;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Database\DatabaseManager;
 
 class LaravelBatchServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind('Batch', function (){
-            return new Batch;
+        $this->app->bind(Batch::class, function ($app){
+            return new Batch($app->make(DatabaseManager::class));
         });
     }
 }
