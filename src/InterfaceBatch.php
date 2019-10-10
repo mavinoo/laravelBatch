fe1eeaf1bc6924c25ce2e1b34c16610e5df1fb7e
Bordin Hongprapat <bordin.hongprapat@gmail.com>
2019-10-10 08:45:13 +0700

    add raw argument in InterfaceBatch

--- src/InterfaceBatch.php
+++ src/InterfaceBatch.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 interface InterfaceBatch
 {
-    public function update(Model $table, array $values, string $index = null);
+    public function update(Model $table, array $values, string $index = null, bool $raw = false);
 
     public function insert(Model $table, array $columns, array $values, int $batchSize = 500);
 }
