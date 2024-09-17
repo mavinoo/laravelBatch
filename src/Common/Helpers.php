de84512bd44c0fb48bdc507bf6880d3f66944b05
Mohammad Ghanbari <mavin.developer@gmail.com>
2024-09-17 14:39:03 +0330

    fixed bug undefined function str()

--- src/Common/Helpers.php
+++ src/Common/Helpers.php
@@ -1,4 +1,8 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Str;
 
 if (!function_exists('batch')) {
     /**
@@ -9,5 +13,33 @@ if (!function_exists('batch')) {
     function batch()
     {
         return app('Mavinoo\Batch\Batch');
+    }
+}
+
+if (!function_exists('str')) {
+    /**
+     * Get a new stringable object from the given string.
+     *
+     * @param  string|null  $string
+     *
+     * @return \Illuminate\Support\Stringable|mixed
+     */
+    function str($string = null)
+    {
+        if (func_num_args() === 0) {
+            return new class {
+                public function __call($method, $parameters)
+                {
+                    return Str::$method(...$parameters);
+                }
+
+                public function __toString()
+                {
+                    return '';
+                }
+            };
+        }
+
+        return Str::of($string);
     }
 }
